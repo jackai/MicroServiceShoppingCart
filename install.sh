1644fd74c00023bc72f6617c6efa5e6b1373dab8
Jacklai <jacklai@program.com.tw>
2018-01-29 13:31:04 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ cp Main/.env.example Main/.env
 cp Product/.env.example Product/.env
 cp CartAndOrder/.env.example CartAndOrder/.env
 
-sudo docker-compose exec Account bash -c "chmod -R 777 bootstrap && chmod -R 777 storage && composer install"
-sudo docker-compose exec Main bash -c "chmod -R 777 bootstrap && chmod -R 777 storage && composer install"
-sudo docker-compose exec Product bash -c "chmod -R 777 bootstrap && chmod -R 777 storage && composer install"
-sudo docker-compose exec CartAndOrder bash -c "chmod -R 777 bootstrap && chmod -R 777 storage && composer install"
+sudo docker-compose exec Account bash -c "chmod -R 777 bootstrap && chmod -R 777 storage && composer install && php artisan key:generate"
+sudo docker-compose exec Main bash -c "chmod -R 777 bootstrap && chmod -R 777 storage && composer install && php artisan key:generate"
+sudo docker-compose exec Product bash -c "chmod -R 777 bootstrap && chmod -R 777 storage && composer install && php artisan key:generate"
+sudo docker-compose exec CartAndOrder bash -c "chmod -R 777 bootstrap && chmod -R 777 storage && composer install && php artisan key:generate"
 

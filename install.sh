c814b0fcbd9c85c9bddc9455ee5de609bab3a09d
Jacklai <jacklai@program.com.tw>
2018-01-29 16:38:27 +0800

    修正docker-compose、install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,28 @@
+apt-get install git -y
+
+# 初始化 submodule
+git submodule init
+git submodule update
+
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose up -d
+
 cp Account/.env.example Account/.env
 cp Main/.env.example Main/.env
 cp Product/.env.example Product/.env
